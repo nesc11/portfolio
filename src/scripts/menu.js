68f728facaa6356aea99b5946095e9434ac1f157
nescpro <salazarnestor1996@gmail.com>
2024-03-01 11:44:14 -0500

    Finish the layout

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger')
 const navLinks = document.querySelector('.nav-links')
+const body = document.body
 hamburger.addEventListener('click', () => {
+    body.classList.toggle('overflow-hidden')
     navLinks.classList.toggle('expanded')
 	hamburger.classList.toggle('expanded')
 })
